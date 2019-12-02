06e44d62e051d6d23e5a5c3a2915872824227023
MysticAdi <adityapt97@gmail.com>
2019-12-02 12:18:39 +0530

    Updates

---
+++ precache-manifest.70a6b14ae4fde190d8681d594c2d2abe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e97d324ad9752cf6209f14b43a7484c",
+    "url": "/monster-doc/index.html"
+  },
+  {
+    "revision": "aea5facb8c9a50ef5f1a",
+    "url": "/monster-doc/static/css/main.8d4a6b55.chunk.css"
+  },
+  {
+    "revision": "66089edaa213f6f86d47",
+    "url": "/monster-doc/static/js/2.d8af18da.chunk.js"
+  },
+  {
+    "revision": "aea5facb8c9a50ef5f1a",
+    "url": "/monster-doc/static/js/main.0d999d75.chunk.js"
+  },
+  {
+    "revision": "822784f8502c1c90d2ec",
+    "url": "/monster-doc/static/js/runtime-main.bbfa2983.js"
+  }
+]);
